73b09b75b86f34ba75106ebc0d35ea882edadee1
Ken Yoon <fenmir@gmail.com>
2014-08-09 19:49:17 +0900

    2nd try

--- plot4.R
+++ plot4.R
@@ -12,14 +12,14 @@ par(mfrow = c(2,2))
 
 plot(powerdata$n.date, Global_active_power, type = 'l', xlab = '', ylab = 'Global Active Power (kilowatts)', main = '')
 
-plot(n.date, Voltage, type = 'l', xlab = 'datetime')
+plot(powerdata$n.date, Voltage, type = 'l', xlab = 'datetime')
 
 plot(powerdata$n.date, Sub_metering_1, type = 'l', xlab = '', ylab = 'Energy sub metering', main = '')
 lines(powerdata$n.date, Sub_metering_2, col = 'red')
 lines(powerdata$n.date, Sub_metering_3, col = 'blue')
 legend('topright', legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lty = c(1, 1, 1), col = c('black', 'red', 'blue'))
 
-plot(n.date, Global_reactive_power, type = 'l', xlab = 'datetime')
+plot(powerdata$n.date, Global_reactive_power, type = 'l', xlab = 'datetime')
 
 detach(powerdata)
 
